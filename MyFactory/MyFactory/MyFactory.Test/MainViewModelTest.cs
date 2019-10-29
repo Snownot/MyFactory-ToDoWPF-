d4776bc7a38402923e902d0b75331aa8e5b77647
AnatoliyPetuhov <anatoliy.p@inspire-tech.com>
2019-10-29 09:18:05 +0700

    start davelop app

---
+++ MyFactory/MyFactory/MyFactory.Test/MainViewModelTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace MyFactory.Test
+{
+    [TestFixture]
+    public class MainViewModelTest : TestBase
+    {
+        [Test]
+        public void Constructor_Pass_Test()
+        {
+        }
+
+        [Test]
+        public void Constructor_Fail_Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ MyFactory/MyFactory/MyFactory.Test/TestBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using NUnit.Framework;
+
+namespace MyFactory.Test
+{
+    [TestFixture]
+    public class TestBase
+    {
+        public TestContext TestContext { get; set; }
+
+        private Stopwatch Stopwatch { get; set; }
+   
+        [SetUp]
+        public void Initialization()
+        {
+            Stopwatch = new Stopwatch();
+            Stopwatch.Start();
+        }
+
+        [TearDown]
+        public void End()
+        {         
+            Stopwatch.Stop();
+            string filename = TestContext.TestDirectory + "/TestsPerformance.txt";
+            string text = "";        
+            if (!File.Exists(filename))
+                text = "Unit Test run on " + DateTime.Now.ToShortDateString() + " "
+                            + DateTime.Now.ToShortTimeString() + Environment.NewLine;
+            text += TestContext.Test + " " + this.Stopwatch.Elapsed.ToString(@"mm\:ss\.fffffff", null);
+            text += " ";
+            File.AppendAllText(filename, text);     
+        }
+    }
+}
